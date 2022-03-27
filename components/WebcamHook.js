9102f79ee40c4c6b237cc58351346777353d4228
Adylan Roaffa <adylan.dev@gmail.com>
2022-03-27 22:50:57 +0800

    add facial expression and bounding box draw

--- components/WebcamHook.js
+++ components/WebcamHook.js
@@ -3,27 +3,33 @@ import Webcam from "react-webcam";
 import * as faceapi from "face-api.js";
 
 let interval = null;
+const webcamWidth = 500;
+const webcamHeight = 500;
+
 
 export default function WebcamHook() {
     const webcamRef = React.useRef(null);
+    const canvasRef = React.useRef(null);
 
     const [img, setImg] = React.useState("")
     const [capturing, setCapturing] = React.useState(false);
     const [faces, setFaces] = React.useState([]);
+    const [expressions, setExpressions] = React.useState([]);
 
-
-    const handleCanvas = React.useCallback(async (canvas) => {
-        console.log(canvas);
+    const handleCanvas = React.useCallback(async (webcamCanvas) => {
         try {
             const detections = await faceapi.detectAllFaces(
-                canvas,
+                webcamCanvas,
                 new faceapi.TinyFaceDetectorOptions()
-              );
-        } catch(e) {
+            ).withFaceExpressions();
+            const detectionsForSize = faceapi.resizeResults(detections, { width: webcamCanvas.width, height: webcamCanvas.height })
+            setFaces(detectionsForSize.map((d) => d.detection.box));
+            setExpressions(detectionsForSize.map((d) => d.expressions));
+
+            } catch (e) {
             console.log(e)
         }
-        
-        setFaces(detections.map((d) => Object.values(d.box)));
+
     }, []);
 
     const capture = React.useCallback(
@@ -40,26 +46,71 @@ export default function WebcamHook() {
         () => {
             clearInterval(interval);
             setCapturing(false);
+            const canvas = canvasRef.current;
+            const canvasContext = canvas.getContext('2d');
+            canvasContext.clearRect(0, 0, canvasContext.canvas.width, canvasContext.canvas.height);
+            canvasContext.beginPath();
         },
         [setCapturing]
     );
 
     React.useEffect(() => {
         const loadModels = () => {
-          Promise.all([
-            faceapi.nets.faceExpressionNet.loadFromUri("/models"),
-          ])
+            Promise.all([
+                faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+                faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+            ])
         };
-    
+
         webcamRef.current && loadModels();
-      }, []);
+    }, []);
+
+    React.useEffect(() => {
+        if (canvasRef !== null) {
+            const canvas = canvasRef.current;
+            const canvasContext = canvas.getContext('2d');
+            canvasContext.clearRect(0, 0, canvasContext.canvas.width, canvasContext.canvas.height);
+            canvasContext.beginPath();
+
+            faces.forEach((face) => {
+                canvasContext.strokeStyle = '#000000'
+                var x = face.x
+                var y = face.y
+                canvasContext.rect(x, y+50, face.width, face.height)
+                canvasContext.stroke();
+            })
+        }
+        console.log(expressions)
+    }, [faces, expressions])
 
     return (
         <>
-            <Webcam
-                audio={false}
-                ref={webcamRef}
-            />
+            <div>
+                <div style={styles.webcam}>
+                    <Webcam
+                        audio={false}
+                        ref={webcamRef}
+                        width={webcamWidth}
+                        height={webcamHeight}
+                        style={{
+                            width: "100%",
+                            objectFit: "fill",
+                            position: "absolute"
+                        }}
+                    />
+                    <canvas 
+                        ref={canvasRef} 
+                        width={webcamWidth}
+                        height={webcamHeight}
+                        style={styles.canvas} />
+                </div>
+                <div>
+                    {expressions.map((expression) => (
+                        <p>{getProbableExpresssions(expression)}</p>
+                    ))}
+                </div>
+            </div>
+
             {capturing ? (
                 <button onClick={stop}>Stop Capture</button>
             ) : (
@@ -67,4 +118,28 @@ export default function WebcamHook() {
             )}
         </>
     )
+}
+
+const getProbableExpresssions = (obj) => {
+    var result = Object.keys(obj).reduce(function(a, b){ return obj[a] > obj[b] ? a : b }, "");
+    console.log(result)
+    return result;
+}
+
+var styles = {
+    container: {
+        height: "calc(100vh - 50px)",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-around"
+    },
+    webcam: {
+        position: "relative",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    canvas: {
+        zIndex: 9999
+    }
 }
